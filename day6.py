46eea5bb6f4c507648c2687ec42bcd272041a263
gilgonzalo <56055261+gilgonzalo@users.noreply.github.com>
2022-12-03 14:51:12 -0800

    Day 6 - elif

---
+++ day6.py
@@ -0,0 +1,28 @@
+print("Gonzalo's SUPER SECURE login")
+username = input("Username : ")
+password = input("Type your password :")
+print("Processing.....")
+
+if username == "Pollis" and password =="amorDeMiVida" :
+  print("""
+      Holis pinche pollis
+      Te amo mucho weeeey
+      I hope this browsing session finds you  well
+
+      Happy coding pinche pollo!""")
+elif username == "Sri" and password == "meLaComoDoblada":
+  print("""
+      Hellotus bitch
+
+      Love you bitch""")
+elif username == "Chuyuyuy" and password == "jeSuisAlmostCanadian":
+  print("""
+      Hello myself
+
+      Workd hard cabron y no te distraigas
+
+      Weeey ya!""")
+
+else:
+  print("""You can't penetrate my super secure system
+            Autodestroying""")
